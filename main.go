4f02f019f32af293dc4e447e6d8b647e58bf3af0
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-23 21:20:01 +0500

    impl base logic

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Использование: downloader <директория> <url1> [url2...]")
+		os.Exit(1)
+	}
+
+	savePath := os.Args[1]
+	urls := os.Args[2:]
+	fmt.Printf("Директория для скачивания: %s\n", savePath)
+	fmt.Println("URL для скачивания")
+	for _, u := range urls {
+		fmt.Printf("\t- %s\n", u)
+	}
 }
